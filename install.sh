a1b218e4b8c1ffd8e2e59e649066a6000fa8cf72
cjohnson714 <cjohnson714@gmail.com>
2025-02-03 04:34:36 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Ensure script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Ensure the script is run from within the nix-config directory (check for flake.nix file)
+if [[ ! -f "flake.nix" ]]; then
+   echo "Error: This script must be run from within the nix-config directory."
+   exit 1
+fi
+
+# Generate the NixOS configuration files
+echo "Generating NixOS configuration..."
+sudo nixos-generate-config
+
+# Replace the hardware-configuration.nix file
+echo "Copying hardware configuration..."
+rm hosts/nixos-vm/hardware-configuration.nix
+cp /etc/nixos/hardware-configuration.nix hosts/nixos-vm/
+
+# Run the nixos-rebuild with flakes, custom substitutor, and experimental features enabled
+echo "Rebuilding NixOS system..."
+sudo nixos-rebuild switch \
+  --flake .#nixos-vm \
+  --option experimental-features "nix-command flakes" \
+  --option extra-substituters "https://chaotic-nyx.cachix.org" \
+  --option extra-trusted-public-keys "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+
+echo "Installation and configuration complete!"
