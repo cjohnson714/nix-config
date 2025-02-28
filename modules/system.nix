70ad844c143dacb87ebedd2dd6ea85e646fd54a3
cjohnson714 <cjohnson714@gmail.com>
2025-02-27 21:56:10 -0500

    chore(system): 🔄 remove commented-out trusted public key
    * Cleaned up `trusted-public-keys` in `system.nix` by removing unnecessary comments.
    * This improves readability and maintains a cleaner configuration file.

--- modules/system.nix
+++ modules/system.nix
@@ -68,7 +68,6 @@
     ]; # Use official Nix cache
 
     trusted-public-keys = [
-      # Included for reference (commented out)
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
       "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
