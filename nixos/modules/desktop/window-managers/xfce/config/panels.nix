40121f35cbc9e7aa35a609b29d7d35f7b867afb2
cjohnson714 <cjohnson714@gmail.com>
2026-05-04 22:02:21 -0400

    fix: Remove xfconf (Home Manager) from XFCE NixOS modules

--- nixos/modules/desktop/window-managers/xfce/config/panels.nix
+++ nixos/modules/desktop/window-managers/xfce/config/panels.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   # XFCE panel configuration
+  # Note: XFCE panel settings should be configured in Home Manager using xfconf.settings
+  
   environment.systemPackages = with pkgs; [
     xfce.xfce4-panel
     xfce.xfce4-pulseaudio-plugin
@@ -8,91 +10,4 @@
     xfce.xfce4-netload-plugin
     xfce.xfce4-battery-plugin
   ];
-
-  # XFCE panel settings
-  xfconf.settings = {
-    panels = {
-      "panel-1" = {
-        "position" = "p=6;x=0;y=0";
-        "length" = "100";
-        "positionx" = "0";
-        "positiony" = "0";
-        "size" = "30";
-        "plugin-ids" = "1,2,3,4,5,6,7,8,9,10";
-        "disable-struts" = false;
-      };
-    };
-
-    plugins = {
-      "plugin-1" = {
-        type = "launcher";
-        "button-style" = "2";
-        "show-button-title" = false;
-        "move-first" = false;
-        "show-tooltip" = true;
-        "item-list" = "firefox.desktop,xfce4-terminal-emulator.desktop,thunar.desktop";
-      };
-
-      "plugin-2" = {
-        type = "tasklist";
-        "grouping" = false;
-        "show-handle" = false;
-        "show-labels" = true;
-        "show-preview" = true;
-      };
-
-      "plugin-3" = {
-        type = "separator";
-        "expand" = true;
-        "style" = "0";
-      };
-
-      "plugin-4" = {
-        type = "systray";
-        "names-hidden" = false;
-        "show-frames" = false;
-        "size-max" = "32";
-        "square-icons" = false;
-      };
-
-      "plugin-5" = {
-        type = "pulseaudio";
-        "enable-keyboard-shortcuts" = true;
-        "show-osd" = true;
-        "mute-on-middle-click" = true;
-      };
-
-      "plugin-6" = {
-        type = "notification";
-        "size-max" = "32";
-        "mouse-left-click" = "default";
-        "mouse-middle-click" = "default";
-        "mouse-right-click" = "default";
-      };
-
-      "plugin-7" = {
-        type = "power-manager-plugin";
-      };
-
-      "plugin-8" = {
-        type = "clock";
-        "mode" = "2";
-        "digital-format" = "%R";
-        "tooltip-format" = "%A %d %B %Y";
-        "show-frame" = false;
-      };
-
-      "plugin-9" = {
-        type = "separator";
-        "expand" = false;
-        "style" = "0";
-      };
-
-      "plugin-10" = {
-        type = "actions";
-        "appearance" = "0";
-        "items" = "logout,lock,suspend,hibernate,reboot,shutdown";
-      };
-    };
-  };
 }
--- nixos/modules/desktop/window-managers/xfce/config/theme.nix
+++ nixos/modules/desktop/window-managers/xfce/config/theme.nix
@@ -3,30 +3,13 @@
   # XFCE-specific theme configuration
   imports = [ ../../shared ];
 
+  # Note: XFCE theme settings should be configured in Home Manager using xfconf.settings
+  # or via a user activation script using xfconf-query
+
   # XFCE-specific theme tools
   environment.systemPackages = with pkgs; [
     xfce.xfce4-settings
     nwg-look
     adw-gtk3
   ];
-
-  # XFCE-specific theme settings
-  xfconf.settings = {
-    xsettings = {
-      "Gtk/ThemeName" = "Catppuccin-Mocha-Standard-Blue-Dark";
-      "Gtk/IconThemeName" = "Papirus-Dark";
-      "Gtk/CursorThemeName" = "Catppuccin-Mocha-Dark-Cursors";
-      "Gtk/FontName" = "SF Pro 10";
-      "Xft/Antialias" = true;
-      "Xft/Hinting" = 1;
-      "Xft/HintStyle" = "hintslight";
-      "Xft/RGBA" = "rgb";
-    };
-    
-    xfwm4 = {
-      "general/theme" = "Catppuccin-Mocha-Standard-Blue-Dark";
-      "general/title_font" = "SF Pro Bold 10";
-      "general/button_layout" = "O|SHMC";
-    };
-  };
 }
